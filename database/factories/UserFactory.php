da14153304baf348268d17a3195f61e6efb4ad8d
acranum1 <Zoloac88H>
2019-10-09 18:09:25 +0300

    init app

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,26 +1,18 @@
 <?php
 
-use App\User;
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
+/** @var EloquentFactory $factory */
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email'          => $faker->unique()->userName,
+        'name'   => $faker->unique()->name,
+        'password'       => 'secret',
         'remember_token' => Str::random(10),
+        'deleted_at'     => $faker->randomElement([null, null, null, Carbon::now()]),
     ];
 });
