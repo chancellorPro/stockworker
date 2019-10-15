8f697e9195657d5154676ba7ff63d367a23878f7
acranum1 <Zoloac88H>
2019-10-15 14:59:05 +0300

    init app

--- app/Http/Controllers/ActionLog/IndexController.php
+++ app/Http/Controllers/ActionLog/IndexController.php
@@ -186,8 +186,10 @@ class IndexController extends Controller
             Mail::send('emails.mail', $emailData, function($message) use ($attach, $orderType) {
                 $message->subject($orderType);
                 $message->from('stockworker100@gmail.com', 'Stock-worker');
-                $message->to('alexander@zolotarev.pp.ua');
-                $message->cc(['cyr@zolotarev.pp.ua', 'pavel@zolotarev.pp.ua']); // garantpak@gmail.com, korreks@meta.ua
+                $message->to('pavel@zolotarev.pp.ua');
+//                $message->to('alexander@zolotarev.pp.ua');
+                $message->cc(['pavel@zolotarev.pp.ua']); // garantpak@gmail.com, korreks@meta.ua
+//                $message->cc(['cyr@zolotarev.pp.ua', 'pavel@zolotarev.pp.ua']); // garantpak@gmail.com, korreks@meta.ua
                 $message->attachData($attach, 'report.xlsx', $options = []);
             });
         } catch (Swift_TransportException $e) {
