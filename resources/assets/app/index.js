e5a80f48e12dcd37e432f7a4b110ecc0ec7a7243
acranum1 <Zoloac88H>
2019-10-16 15:39:41 +0300

    init app

--- resources/assets/app/index.js
+++ resources/assets/app/index.js
@@ -50,16 +50,6 @@ import './styles/index.scss';
 
 import Raven from 'raven-js';
 
-if (SENTRY_DSN && SENTRY_DSN.length) {
-    Raven.config(SENTRY_DSN, {
-        xhr: true,
-        console: true,
-        dom: true,
-        location: false,
-        sentry: true
-    }).install();
-}
-
 /**
  * Modules loader
  */
