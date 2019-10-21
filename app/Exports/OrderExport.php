3816402e611a95ea6b4bf2b2d5a490e9b63de202
chancellorPro <pavel@zolotarev.pp.ua>
2019-10-21 20:51:14 +0300

    update mail recipient

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -34,13 +34,16 @@ class OrderExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        $builder = ActionLog::selectRaw('action_log.product_id, max(p.box_id) as box_id, max(p.box_size) as box_size, max(p.box_weight) as box_weight, max(p.name) as p_name, max(s.count) as s_count, max(pl.count) as pl_count, sum(action_log.count) as al_count, max(c.name) as c_name')
+        $builder = ActionLog::selectRaw('action_log.product_id, max(p.box_id) as box_id, max(p.box_size) as box_size, 
+        max(p.box_weight) as box_weight, max(p.name) as p_name, max(s.count) as s_count, max(pl.count) as pl_count, 
+        sum(action_log.count) as al_count, max(c.name) as c_name')
             ->leftJoin('products AS p', 'p.id', '=', 'action_log.product_id')
             ->leftJoin('plan AS pl', 'pl.product_id', '=', 'action_log.product_id')
             ->leftJoin('customers AS c', 'c.id', '=', 'action_log.customer_id')
             ->leftJoin('stock AS s', 'p.id', '=', 's.product_id')
             ->whereBetween('action_log.date', [$this->from, $this->to])
-            ->where(['income' => $this->income]);
+            ->where(['income' => $this->income])
+            ->groupBy('c.id');
 
         $parentIds = Product::selectRaw('distinct parent_product')->get()->pluck('parent_product')->toArray();
 
