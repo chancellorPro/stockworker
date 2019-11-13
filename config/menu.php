2c9912fcc810c6b5a77b5996b13ba91c2a50af80
pavel <pavel@zolotarev.pp.ua>
2019-11-13 18:03:21 +0200

    send to telegram bot

--- config/menu.php
+++ config/menu.php
@@ -20,6 +20,7 @@ return [
         'name' => 'Admin',
         'child' => [
             ['name' => 'События', 'route' => 'action-log.index', 'icon' => 'fa-dropbox'],
+            ['name' => 'План', 'route' => 'plan.index', 'icon' => 'fa-bullhorn'],
             ['name' => 'История', 'route' => 'history', 'icon' => 'fa-history'],
             ['name' => 'Склад', 'route' => 'stock.index', 'icon' => 'fa-th'],
             ['name' => 'Товары', 'route' => 'product.index', 'icon' => 'fa-cubes'],
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -11,36 +11,36 @@
 
         <!-- menu profile quick info -->
         <div class="profile">
-            
+
             <div class="profile_info">
                 <h2>@lang('Welcome'), {{ Auth::user()->name }}</h2>
             </div>
         </div>
         <!-- /menu profile quick info -->
-        
+
         <br />
-        
+
         <!-- sidebar menu -->
-        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-            <div class="menu-section">
-                <ul class="nav side-menu">
-                    @foreach(config('menu') as $menuItem)
-                        @if (isset($menuItem['child']))
-                            @foreach($menuItem['child'] as $childItem)
-                                <li>
-                                    <a href="{{ route($childItem['route'], (array)@$childItem['route_params']) }}">
-                                        <i class="fa {{ $childItem['icon'] }}"></i>
-                                        {{ $childItem['name'] }}
-                                    </a>
-                                </li>
-                            @endforeach
-                        @endif
-                    @endforeach
-                </ul>
-            </div>
-        </div>
+{{--        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">--}}
+{{--            <div class="menu-section">--}}
+{{--                <ul class="nav side-menu">--}}
+{{--                    @foreach(config('menu') as $menuItem)--}}
+{{--                        @if (isset($menuItem['child']))--}}
+{{--                            @foreach($menuItem['child'] as $childItem)--}}
+{{--                                <li>--}}
+{{--                                    <a href="{{ route($childItem['route'], (array)@$childItem['route_params']) }}">--}}
+{{--                                        <i class="fa {{ $childItem['icon'] }}"></i>--}}
+{{--                                        {{ $childItem['name'] }}--}}
+{{--                                    </a>--}}
+{{--                                </li>--}}
+{{--                            @endforeach--}}
+{{--                        @endif--}}
+{{--                    @endforeach--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <!-- /sidebar menu -->
-        
+
         <!-- /menu footer buttons -->
 <!--        <div class="sidebar-footer hidden-small">
             <a>
