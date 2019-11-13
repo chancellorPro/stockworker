3db55a032f13b892f27ddd1683373f1146e5b179
pavel <pavel@zolotarev.pp.ua>
2019-11-13 18:04:59 +0200

    send to telegram bot

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -24,7 +24,6 @@
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu-section">
                 <ul class="nav side-menu">
-                    @dd(config('menu'))
                     @foreach(config('menu') as $menuItem)
                         @if (isset($menuItem['child']))
                             @foreach($menuItem['child'] as $childItem)
